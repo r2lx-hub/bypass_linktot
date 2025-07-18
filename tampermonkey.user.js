56cb25adb4f516a24b68885e4a1da37a92c692e6
r2lx-hub <r2lxhub@gmail.com>
2025-07-18 18:27:49 +0700

    Add files via upload

---
+++ tampermonkey.user.js
@@ -0,0 +1,11 @@
+// ==UserScript==
+// @name         Bypass Linktot.net Unlocker (Tool)
+// @namespace    http://tampermonkey.net/
+// @version      Beta
+// @description  Auto vượt Linktot.net không cần chạm tay
+// @author       By R2lx
+// @require      https://raw.githubusercontent.com/r2lx-hub/bypass_linktot/refs/heads/main/bypass_linktot.js
+// @match        https://linktot.net/*
+// @grant        none
+// @icon         
+// ==/UserScript==
